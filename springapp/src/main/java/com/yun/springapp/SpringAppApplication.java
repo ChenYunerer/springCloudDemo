a7872030e3b4eba4b2c801e8b4207a2497fa1c06
Yun <yun160308@gmail.com>
2019-07-17 23:02:45 +0800

    feat(分布式事务): 增加LCN分布式事务管理

--- springapp/src/main/java/com/yun/springapp/SpringAppApplication.java
+++ springapp/src/main/java/com/yun/springapp/SpringAppApplication.java
@@ -1,11 +1,13 @@
 package com.yun.springapp;
 
+import com.codingapi.txlcn.tc.config.EnableDistributedTransaction;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableEurekaClient
 @SpringBootApplication
+@EnableDistributedTransaction
 public class SpringAppApplication {
 
     public static void main(String[] args) {
--- springapp3/src/main/java/com/yun/springapp3/SpringApp3Application.java
+++ springapp3/src/main/java/com/yun/springapp3/SpringApp3Application.java
@@ -1,11 +1,13 @@
 package com.yun.springapp3;
 
+import com.codingapi.txlcn.tc.config.EnableDistributedTransaction;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableEurekaClient
 @SpringBootApplication
+@EnableDistributedTransaction
 public class SpringApp3Application {
 
     public static void main(String[] args) {
