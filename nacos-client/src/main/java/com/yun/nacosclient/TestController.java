ff1fa35b77f792fa4f3fcf3a42f716b2ba533a6c
Yun <yuner160308@gmail.com>
2020-08-03 22:48:38 +0800

    feat(seata): 增加seata client

--- nacos-client/src/main/java/com/yun/nacosclient/TestController.java
+++ nacos-client/src/main/java/com/yun/nacosclient/TestController.java
@@ -2,10 +2,12 @@ package com.yun.nacosclient;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RefreshScope
 @RequestMapping("/test")
 @RestController
 public class TestController {
---
+++ seata-client/src/main/java/com/yuner/seataclient/SeataClientApplication.java
@@ -0,0 +1,13 @@
+package com.yuner.seataclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeataClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeataClientApplication.class, args);
+    }
+
+}
